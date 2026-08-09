fc63f385c7a6cc944c973e10cfc2afe6efc00d02
Alzhana Diyas <lkipermanl@gmail.com>
2026-08-09 17:32:03 +0300

    Connect FastAPI to PostgreSQL

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,20 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from database import get_db
+from models import City
+
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Travel Planner API is working"}
+    return {"message": "Odysseus API is working"}
 
 
 @app.get("/cities")
-def get_cities():
-    return [
-        {"id": 1, "name": "Riga", "country": "Latvia"},
-        {"id": 2, "name": "Istanbul", "country": "Turkey"},
-        {"id": 3, "name": "Barcelona", "country": "Spain"},
-    ]
-    
+def get_cities(db: Session = Depends(get_db)):
+    cities = db.query(City).all()
+
+    return cities
---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "cities"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    country: Mapped[str] = mapped_column(String(100), nullable=False)
